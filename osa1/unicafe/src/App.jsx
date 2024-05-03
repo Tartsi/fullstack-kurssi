5cc0b7f99f02be40a6570c4bdebf369516fb6153
Tartsi <tarvo.lilja@hotmail.fi>
2024-05-03 03:01:57 +0300

    unicafe step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -17,29 +17,38 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
+  const [goodValue, setGoodValue] = useState(0);
+  const [badValue, setBadValue] = useState(0);
+  const [neutralValue, setNeutralValue] = useState(0);
   
   const handleGoodClick = () => {
     setGood(good + 1)
+    setGoodValue(goodValue + 1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setBadValue(badValue - 1)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setNeutralValue(neutralValue)
   }
 
   return (
     <div>
       <Header text="give feedback" />
       <Button handleClick={handleGoodClick} text="good" />
-      <Button handleClick={handleBadClick} text="bad" />
       <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <Header text="statistics" />
       <p>good {good}</p>
-      <p>bad {bad}</p>
       <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>total {good+bad+neutral}</p>
+      <p>average {(goodValue+neutralValue+badValue)/(good+bad+neutral)}</p>
+      <p>positive {good/(good+bad+neutral)*100} %</p>
     </div>
   )
 }
