ad1be8a6c8f4c5b33fb998a1173bee672cf00981
Tartsi <tarvo.lilja@hotmail.fi>
2025-03-08 02:16:22 +0200

    4.6* apufunktioita ja yksikkötestejä, step4

--- osa4/tests/list_helper.test.js
+++ osa4/tests/list_helper.test.js
@@ -143,3 +143,63 @@ describe('favorite blog', () => {
     assert.strictEqual(result, listWithMultipleBlogs[0])
   }
 })
+
+describe('most blogs', () => {
+  
+  const emptyList = []
+
+  test('of empty list is empty object'), () => {
+    const result = listHelper.mostBlogs(emptyList)
+    assert.strictEqual(result, {})
+  }
+
+  test('when list has only one blog equals the author of that'), () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    assert.strictEqual(result, { author: listWithOneBlog[0].author, blogs: 1 })
+  }
+
+  test('when list has multiple blogs equals the author of that'), () => {
+    const result = listHelper.mostBlogs(listWithMultipleBlogs)
+    assert.strictEqual(result, { author: 'Robert C. Martin', blogs: 3 })
+  }
+
+  const listWithMultipleSimilarLikedBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 261121,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Michael Chan",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 26234,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 2612,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d173453453535353f9",
+      title: "Canonical string reduction vol 2",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 26545754,
+      __v: 0
+    },
+  ]
+
+  test('when list has multiple blogs with same amount of blogs return one of them'), () => {
+    const result = listHelper.mostBlogs(listWithMultipleSimilarLikedBlogs)
+    assert.strictEqual(result, { author: 'Michael Chan', blogs: 1 })
+  }
+})
--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -10,8 +10,24 @@ const favoriteBlog = (blogs) => {
     return blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
 }
 
+const mostBlogs = (blogs) => {
+    const authors = blogs.map(blog => blog.author)
+    const author = authors.reduce((prev, current) => {
+        prev[current] = (prev[current] || 0) + 1
+        return prev
+    }, {})
+
+    const authorWithMostBlogs = Object.keys(author).reduce((prev, current) => author[prev] > author[current] ? prev : current)
+
+    return {
+        author: authorWithMostBlogs,
+        blogs: author[authorWithMostBlogs]
+    }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
 }
