4fdf5e26e69385ac8f48fa89a93dad90cf730bb7
Tartsi <tarvo.lilja@hotmail.fi>
2025-04-12 01:13:00 +0300

    4.19: blogilistan laajennus, step7

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,15 +18,24 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const { title, url, author, likes } = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+  if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  
+  const user = await User.findById(decodedToken.id)
+
+  if (!user) {
+    return response.status(401).json({ error: 'invalid or missing user' })
+  }
+
 
   if (!title || !url) {
       return response.status(400).json({ error: 'title or url is missing' })
   }
 
-  const user = await User.findById(request.body.user)
-  if (!user) {
-      return response.status(401).json({ error: 'invalid or missing user' })
-  }
+
 
   const blog = new Blog({
       title,
