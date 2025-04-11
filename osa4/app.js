0d524db037e4f355ab5943528dcd03d3176c3659
Tartsi <tarvo.lilja@hotmail.fi>
2025-04-12 01:22:26 +0300

    4.20*: blogilistan laajennus, step8

--- osa4/app.js
+++ osa4/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const middleware = require('./utils/middleware')
 
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
@@ -24,9 +25,14 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -18,7 +18,7 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const { title, url, author, likes } = request.body
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
   if (!decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' })
