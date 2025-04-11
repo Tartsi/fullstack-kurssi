8bc731d19c4606ba969c9b0cff5afbeaa99422d1
Tartsi <tarvo.lilja@hotmail.fi>
2025-04-12 02:00:05 +0300

    4.22*: blogilistan laajennus, step10 + add formatting

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,103 +1,92 @@
-const blogsRouter = require('express').Router()
-const jwt = require('jsonwebtoken')
-const Blog = require('../models/blog')
-const User = require('../models/user')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const { tokenExtractor, userExtractor } = require("../utils/middleware");
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+  });
+  response.json(blogs);
+});
 
-blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-    response.json(blogs)
-})
+blogsRouter.post(
+  "/",
+  tokenExtractor,
+  userExtractor,
+  async (request, response) => {
+    const { title, url, author, likes } = request.body;
+    const user = request.user;
 
-blogsRouter.post('/', async (request, response) => {
-  const { title, url, author, likes } = request.body
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!title || !url) {
+      return response.status(400).json({
+        error: "title or url is missing",
+      });
+    }
 
-  if (!decodedToken.id) {
-      return response.status(401).json({ error: 'token missing or invalid' })
-  }
-  
-  const user = await User.findById(decodedToken.id)
-
-  if (!user) {
-    return response.status(401).json({ error: 'invalid or missing user' })
-  }
-
-
-  if (!title || !url) {
-      return response.status(400).json({ error: 'title or url is missing' })
-  }
-
-
-
-  const blog = new Blog({
+    const blog = new Blog({
       title,
       author,
       url,
       likes: likes || 0,
-      user: user._id
-  })
+      user: user._id,
+    });
 
-  const savedBlog = await blog.save()
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
 
-  // Optionally add blog to the user's list and save
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
-
-  response.status(201).json(savedBlog)
-})
-  
-blogsRouter.delete('/:id', async (request, response) => {
-  const authorization = request.get('authorization')
-
-  if (!authorization || !authorization.toLowerCase().startsWith('bearer ')) {
-      return response.status(401).json({ error: 'token missing or invalid' })
+    response.status(201).json(savedBlog);
   }
+);
 
-  const token = authorization.substring(7)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+blogsRouter.delete(
+  "/:id",
+  tokenExtractor,
+  userExtractor,
+  async (request, response) => {
+    const user = request.user;
+    const blog = await Blog.findById(request.params.id);
 
-  if (!decodedToken.id) {
-      return response.status(401).json({ error: 'token invalid' })
+    if (!blog) {
+      return response.status(404).json({
+        error: "blog not found",
+      });
+    }
+
+    if (blog.user.toString() !== user._id.toString()) {
+      return response.status(403).json({
+        error: "unauthorized: you can only delete your own blogs",
+      });
+    }
+
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
   }
+);
 
-  const blog = await Blog.findById(request.params.id)
-
-  if (!blog) {
-      return response.status(404).json({ error: 'blog not found' })
-  }
-
-  if (blog.user.toString() !== decodedToken.id.toString()) {
-      return response.status(403).json({ error: 'unauthorized: you can only delete your own blogs' })
-  }
-
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-})
-
-blogsRouter.put('/:id', async (request, response) => {
-
-  const { title, author, url, likes } = request.body
+blogsRouter.put("/:id", async (request, response) => {
+  const { title, author, url, likes } = request.body;
 
   const updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
-    { title, author, url, likes },
-    { new: true, runValidators: true }
-  )
+    {
+      title,
+      author,
+      url,
+      likes,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
 
   if (updatedBlog) {
-    response.json(updatedBlog)
+    response.json(updatedBlog);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
+});
 
-})
-
-module.exports = blogsRouter
+module.exports = blogsRouter;
