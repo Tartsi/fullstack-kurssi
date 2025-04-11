2a05746538b992624850c77d9a7573a340564dce
Tartsi <tarvo.lilja@hotmail.fi>
2025-04-12 00:57:03 +0300

    4.18: blogilistan laajennus, step6

--- osa4/app.js
+++ osa4/app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -26,5 +27,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
