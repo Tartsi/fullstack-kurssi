ebec5c63df354688c15c129b0ce3957030257ce8
Tartsi <tarvo.lilja@hotmail.fi>
2025-05-14 15:08:03 +0300

    blogilistan frontend, step1

--- osa5/src/App.jsx
+++ osa5/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -12,9 +13,31 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  });
+
   const handleLogin = async (event) => {
     event.preventDefault();
-    console.log("Logging in with", username, password);
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.log("[DEBUG] Error occured when logging in!");
+      console.log(exception);
+    }
   };
 
   const loginForm = () => (
@@ -44,10 +67,15 @@ const App = () => {
   return (
     <div>
       {!user && loginForm()}
-      <h2>blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {user && (
+        <>
+          <h2>blogs</h2>
+          <p>{user.name} logged in </p>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
