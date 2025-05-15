fea09181f230b7108f30776734c95c48cec46e92
Tartsi <tarvo.lilja@hotmail.fi>
2025-05-15 14:39:53 +0300

    5.3: blogilistan frontend, step3

--- osa5/src/App.jsx
+++ osa5/src/App.jsx
@@ -7,6 +7,9 @@ const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [blogUrl, setBlogUrl] = useState("");
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -20,6 +23,29 @@ const App = () => {
       setUser(user);
     }
   }, []);
+
+  const addBlog = async (event) => {
+    event.preventDefault();
+
+    try {
+      const blogObject = {
+        title: title,
+        author: author,
+        url: blogUrl,
+        user: user,
+      };
+
+      console.log("Sent blogObject:", blogObject);
+      const returnedObject = await blogService.create(blogObject);
+      setBlogs(blogs.concat(returnedObject));
+      setTitle("");
+      setAuthor("");
+      setBlogUrl("");
+    } catch (exception) {
+      console.log("Error occured when adding notes!");
+      console.log(exception);
+    }
+  };
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -39,6 +65,7 @@ const App = () => {
     } catch (exception) {
       console.log("[DEBUG] Error occured when logging in!");
       console.log(exception);
+      alert("Error when logging in! Reason: " + exception.response.data.error);
     }
   };
 
@@ -78,6 +105,41 @@ const App = () => {
     </form>
   );
 
+  const noteForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        <div>
+          title:
+          <input
+            type="text"
+            value={title}
+            name=""
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            value={author}
+            name=""
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            value={blogUrl}
+            name=""
+            onChange={({ target }) => setBlogUrl(target.value)}
+          />
+        </div>
+      </div>
+      <button type="submit">create</button>
+    </form>
+  );
+
   return (
     <div>
       {!user && loginForm()}
@@ -86,6 +148,8 @@ const App = () => {
           <h2>blogs</h2>
           <p>{user.name} logged in </p>
           {logOutForm()}
+          <h2>create new</h2>
+          {noteForm()}
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
