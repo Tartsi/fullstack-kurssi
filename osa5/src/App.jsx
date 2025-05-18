6d0be752889a102e7577051460cc78aabccf112f
Tartsi <tarvo.lilja@hotmail.fi>
2025-05-19 00:52:17 +0300

    5.10: blogilistan frontend, step10

--- osa5/src/App.jsx
+++ osa5/src/App.jsx
@@ -156,9 +156,11 @@ const App = () => {
           <h2>create new</h2>
           {noteForm()}
           <br />
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} likeBlog={likeBlog} user={user} />
-          ))}
+          {[...blogs]
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+              <Blog key={blog.id} blog={blog} likeBlog={likeBlog} user={user} />
+            ))}
         </>
       )}
     </div>
