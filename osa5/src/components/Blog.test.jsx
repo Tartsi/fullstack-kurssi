f588bd3b04718543ec801f08016de233b3c72b13
Tartsi <tarvo.lilja@hotmail.fi>
2025-05-23 16:08:01 +0300

    5.14: blogilistan testit, step2

--- osa5/src/components/Blog.test.jsx
+++ osa5/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 test("blog displays only title and author initially", () => {
@@ -22,10 +23,30 @@ test("blog displays only title and author initially", () => {
     />
   );
 
-  console.log("Debug here:");
-  screen.debug();
-
   expect(screen.getByText(/Test Blog/)).toBeInTheDocument();
   expect(screen.getByText(/Test Author/)).toBeInTheDocument();
   expect(screen.queryByText(/testblog.com/)).not.toBeInTheDocument();
+});
+
+test("blog displays url, likes and user after 'view'-button pressed", async () => {
+  const blog = {
+    title: "Test Blog",
+    author: "Test Author",
+    url: "testblog.com",
+    likes: 15,
+    user: { username: "testuser" },
+  };
+
+  render(<Blog blog={blog} likeBlog={() => {}} deleteBlog={() => {}} />);
+
+  const userPress = userEvent.setup();
+  const button = screen.getByText("view");
+  await userPress.click(button);
+
+  console.log("Debug here:");
+  screen.debug();
+
+  expect(screen.getByText(/testblog.com/)).toBeInTheDocument();
+  expect(screen.getByText(/likes 15/)).toBeInTheDocument();
+  expect(screen.getByText(/testuser/)).toBeInTheDocument();
 });
